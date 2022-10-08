83f063af702b92fd5e53e022bfca7a45f8822377
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-08 20:08:06 +0100

    feat: add accounts card

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -27,8 +27,134 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(),
-      extendBodyBehindAppBar: true,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Overview',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const AccountsCard(),
+            const AccountsCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+@visibleForTesting
+class AccountsCard extends StatelessWidget {
+  const AccountsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.width * 0.5,
+      margin: EdgeInsets.only(
+        top: MediaQuery.of(context).size.width * 0.05,
+      ),
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Accounts',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextButton(
+                child: Text(
+                  'View all',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    child: Icon(Icons.list_alt, color: Colors.white, size: 50),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 1,
+                      horizontal: 8,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: const Text(
+                      'Checkings',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                '£1,130.00',
+                style: TextStyle(fontSize: 17),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                child: Text('Add Account'),
+                style: ButtonStyle(
+                  backgroundColor: Colors.grey,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
