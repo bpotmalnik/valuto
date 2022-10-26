e78f87d41dc773052e2bc9a20cae552c568328ec
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-26 22:35:07 +0100

    feat: update AppView to accept repostories

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,18 +1,36 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
+import 'package:accounts_repository/accounts_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:valuto/counter/counter.dart';
+import 'package:transactions_repository/transactions_repository.dart';
 import 'package:valuto/home/view/home_page.dart';
 import 'package:valuto/l10n/l10n.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({
+    required AccountsRepository accountsRepository,
+    required TransactionsRepository transactionsRepository,
+    super.key,
+  }) : _accountsRepository = accountsRepository,
+    _transactionsRepository = transactionsRepository;
+
+  final AccountsRepository _accountsRepository;
+  final TransactionsRepository _transactionsRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+         RepositoryProvider.value(value: _accountsRepository),
+         RepositoryProvider.value(value: _transactionsRepository),
+      ],
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,13 +1,24 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
+import 'package:accounts_repository/accounts_repository.dart';
+import 'package:api_client/api_client.dart';
+import 'package:transactions_repository/transactions_repository.dart';
 import 'package:valuto/app/app.dart';
 import 'package:valuto/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  final apiClient = ApiClient();
+
+  final accountsRepository = AccountsRepository(
+    apiClient: apiClient,
+  );
+
+  final transactionsRepository = TransactionsRepository(
+    apiClient: apiClient,
+  );
+
+  bootstrap(
+    () => App(
+      accountsRepository: accountsRepository,
+      transactionsRepository: transactionsRepository,
+    ),
+  );
 }
