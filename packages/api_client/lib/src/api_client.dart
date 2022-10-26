d49523ed2f8318ce9b7d41dd6f3084d52ac4444f
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-26 15:46:35 +0100

    feat: add accounts resource

--- packages/api_client/lib/src/api_client.dart
+++ packages/api_client/lib/src/api_client.dart
@@ -1,14 +1,18 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+import 'package:api_client/src/resources/accounts_resource.dart';
+import 'package:http/http.dart' as http;
 
 /// {@template api_client}
 /// API client
 /// {@endtemplate}
 class ApiClient {
   /// {@macro api_client}
-  const ApiClient();
+  const ApiClient({http.Client? httpClient})
+          : _httpClient = httpClient ?? http.Client();
+
+  final http.Client _httpClient;
+
+  /// access property for [AccountsResource]
+  late final AccountsResource accountsResource = 
+          AccountsResource(httpClient: _httpClient);
 }
+
